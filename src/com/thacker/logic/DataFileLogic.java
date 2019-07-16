d62f7ffd465b3161ef6f9a58f09474d170ebca7a
James <Thacker>
2019-07-16 09:51:56 -0400

    Added fileinput/output variables

--- src/com/thacker/logic/DataFileLogic.java
+++ src/com/thacker/logic/DataFileLogic.java
@@ -1,4 +1,16 @@
 package com.thacker.logic;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 public class DataFileLogic {
+
+    /**
+     * Class handles all operations on the data file
+     */
+
+    private FileInputStream fin = null;
+    private FileOutputStream fout = null;
+
+    
 }
